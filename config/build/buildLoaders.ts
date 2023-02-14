a5f29ccd4a518294c2bb2382ac95f2ad0dada088
devDalys <idalys1357@gmail.com>
2023-02-14 22:15:27 +0300

    fix build git

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,37 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {buildOptions} from "./types/config";
+
+export function buildLoaders(options: buildOptions): webpack.RuleSetRule[] {
+    const cssLoader: webpack.RuleSetRule = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (respath: string) => respath.includes('.module.scss.'),
+                        localIdentName: options.isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
+                    },
+
+                }
+            },
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    }
+
+    const tsloader: webpack.RuleSetRule = {
+        test: /\.tsx?$/, //Регулярка для выборки файлов
+        use: 'ts-loader', // лоадер для обработки
+        exclude: /node_modules/,
+    }
+
+    return [
+        tsloader,
+        cssLoader
+    ]
+}
