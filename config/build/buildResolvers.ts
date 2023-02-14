f6ac277b7bb7a8cf0c0d71973e83e728d05a1c44
devDalys <idalys1357@gmail.com>
2023-02-15 00:02:46 +0300

    apx-py architecture

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,5 +1,12 @@
 import {ResolveOptions} from "webpack";
+import {buildOptions} from "./types/config";
 
-export function buildResolvers(): ResolveOptions {
-    return {extensions: ['.tsx', '.ts', '.js']}
+export function buildResolvers(options: buildOptions): ResolveOptions {
+    return {
+        extensions: ['.tsx', '.ts', '.js'],
+        preferAbsolute: true,
+        modules:[options.paths.src, 'node_modules'],
+        mainFiles:['index'],
+        alias:{}
+    }
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
-import React, {Suspense, useContext} from 'react';
+import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss'
-import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
-import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
-import {useTheme} from "./theme/useTheme";
-import {cn} from "./helpers/classNames/classNames";
+import {cn} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider/lib/useTheme";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 
 
@@ -18,8 +18,8 @@ const App = () => {
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={'Loading...'}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageLazy/>}/>
-                    <Route path={'/'} element={<MainPageLazy/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
 
