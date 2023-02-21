c5be18ceaec6676a4af66cc23a952512a2f90980
devDalys <idalys1357@gmail.com>
2023-02-22 00:03:50 +0300

    babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -18,6 +18,26 @@ export function buildLoaders(options: buildOptions): webpack.RuleSetRule[] {
         ],
     }
 
+    const babelLoader = {
+        test: /\.(js|tsx|ts)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    ["i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ],
+                ]
+            },
+
+        }
+    }
+
     const cssLoader: webpack.RuleSetRule = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -48,6 +68,7 @@ export function buildLoaders(options: buildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         tsloader,
         cssLoader,
     ]
